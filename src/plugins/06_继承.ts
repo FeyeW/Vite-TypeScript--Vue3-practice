950d0ec2ea13f341482b9af677077d5c29b98aa4
FeyeW <412755014@qq.com>
2022-07-19 14:31:41 +0800

    feat: 增加抽象类的概念内容

--- src/plugins/06_继承.ts
+++ src/plugins/06_继承.ts
@@ -16,6 +16,27 @@
     }
    }
 
+   //定义一个抽象类
+   /* 
+     以abstracta开头的类就是抽象类
+       抽象类和其他类的区别不大，只是不能用来创建对象
+       抽象类就是专门用来被继承的类
+
+   */
+   abstract class Animals{
+    name:string
+    constructor(name:string)
+    {
+        this.name=name
+    }
+    /* 
+    定义一个抽象方法
+    抽象方法使用 3abstract开头，没有方法体
+    抽象方法只能定义在抽象类中，子类必须对抽象方法进行重写
+    */
+   abstract sayHello():void
+   }
+   
    /* 
     Dong extends Animal
        - 此时，Animal被称为父类，Dog被称为子类
@@ -28,19 +49,25 @@
    */
    //定义一个表示狗的类
    class Dog extends Animal{
+    age:number
+    constructor(name:string,age:number)
+    {
+        super(name,age) //调用父类的构造函数
+        this.age=age
+    }
        run()
        {
         console.log(`${this.name}在跑...`)
        }
    }
 
-   class Cat extends Animal{
+   class Cat extends Animals{
      sayHello() {
          console.log('喵喵喵')
      }
    }
 
    const dog=new Dog('旺财',12)
-   const cat=new Cat('咪咪',3)
+   const cat=new Cat('咪咪')
    console.log(cat.sayHello)
 })()
